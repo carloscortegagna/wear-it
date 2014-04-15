7e973ce788f9a71330f099c8800ac758abbdb63f
Carlo Scortegagna <carlo.scortegagna@develon.com>
2014-04-15 16:26:40 +0200

    new spree 2.2 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WearIt::Application.config.secret_key_base = 'ad3fcb31a9f04cb616b3c68bc7140eda215262fd010f015c2b1801e9cc0938863a3d0841d442d4033e604c99ac4ce111dc146bf61d63260c741278b1a8375229'
